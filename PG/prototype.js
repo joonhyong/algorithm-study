5175965a106fa4b0bd4fc8f2beaeba373f23b74c
joonhyong <dbshwns0528@naver.com>
2026-08-25 01:30:40 +0900

    [docs] prototype.js - solution.js 초기 코드 파일 생성

---
+++ PG/prototype.js
@@ -0,0 +1,26 @@
+/*
+# 🧩 문제 정보
+사이트: Programmers
+레벨: Level 0
+문제명: 
+유형: 
+날짜: 2026-08-25
+Review 여부: false
+
+# 📰 문제 설명
+
+# 💡 문제 풀이
+
+# ⏰ 시간복잡도 O(N)
+
+# 🚀 알게 된 점
+
+# 💭 아쉬운 점
+
+*/
+
+function solution(a) {
+  return answer;
+}
+
+console.log(solution(5));
